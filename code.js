dcfeb3fc83113cec6c5c52952ccd4a17308f71cf
Dimos Michailidis <dimos86m@gmail.com>
2023-03-01 21:07:53 +0200

    Appropriate modulo for color index

--- code.js
+++ code.js
@@ -34,7 +34,7 @@ function addNode(action, maxNodeWeight) {
         x: 0.5 * 1080.0 * (0.5 - Math.random()),
         y: 0.5 * 1080.0 * (0.5 - Math.random()),
         font: font(action, maxNodeWeight),
-        color: colors[action.cluster % 20]
+        color: colors[action.cluster % 19]
     });
 }
 
